57973b7fd0e9eebc63a4d2b31bc5c427afb83bee
Yu Chong Ansel Goh <s10266842@connect.np.edu.sg>
2025-01-16 15:44:55 +0800

    class

---
+++ PRG2-T13-01/CFFTFlight.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_T13_01
+{
+    class CFFTFlight : Flight
+    {
+        public double RequestFee { get; set; }
+        public CFFTFlight(string fn, string o, string d, DateTime et, string s, double rf) : base(fn, o, d, et, s)
+        {
+            RequestFee = rf;
+        }
+        public override double CalculateFees()
+        {
+            RequestFee = 150;
+            if (Origin == "Singapore")
+            {
+                double fees = RequestFee + 800;
+                return fees;
+            }
+            else if (Destination == "Singapore")
+            {
+                double fees = RequestFee + 500;
+                return fees;
+            }
+            else
+            {
+                return 0.0;
+            }
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $", Type: CFFT Flight, Additional Fee: {RequestFee}";
+        }
+    }
+}
