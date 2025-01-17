81ccea95ccc1c190ecce239dc3a7f62622b6f793
Yu Chong Ansel Goh <s10266842@connect.np.edu.sg>
2025-01-17 14:26:16 +0800

    airline

---
+++ PRG2-T13-01/Airline.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_T13_01
+{
+    class Airline
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public Dictionary<string, Flight> Flights { get; set; }
+
+        public Airline(string code, string name)
+        {
+            Code = code;
+            Name = name;
+            Flights = new Dictionary<string, Flight>();
+        }
+        public void AddFlight(Flight flight)
+        {
+            Flights[flight.FlightNumber] = flight;
+        }
+        public double CalculateFees()
+        {
+            double fees = 0;
+            foreach (Flight flight in Flights.Values)
+            {
+                fees += flight.CalculateFees();
+            }
+            return fees;
+
+        }
+        public override string ToString()
+        {
+            return $"{Name} ({Code})";
+        }
+    }
+}
