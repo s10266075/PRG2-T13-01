597bc7e106f5fd05e4add56ef817ca2c64583cf7
Yu Chong Ansel Goh <s10266842@connect.np.edu.sg>
2025-01-25 22:57:54 +0800

    remove display menu

--- PRG2-T13-01/Program.cs
+++ PRG2-T13-01/Program.cs
@@ -45,21 +45,6 @@ void LoadBoardingGates()
         }
     }
 }
-//display menu
-void DisplayMenu()
-{
-    Console.WriteLine("=============================================");
-    Console.WriteLine("Welcome to Changi Airport Terminal 5");
-    Console.WriteLine("=============================================");
-    Console.WriteLine("1. List All Flights");
-    Console.WriteLine("2. List Boarding Gates");
-    Console.WriteLine("3. Assign a Boarding Gate to a Flight");
-    Console.WriteLine("4. Create Flight");
-    Console.WriteLine("5. Display Airline Flights");
-    Console.WriteLine("6. Modify Flight Details");
-    Console.WriteLine("7. Display Flight Schedule");
-    Console.WriteLine("0. Exit");
-}
 
 //feature 4
 
