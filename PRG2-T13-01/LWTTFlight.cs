27eb86079b14c076372952b464c5588d4395f4e1
Day Gene Low <s10266075@connect.np.edu.sg>
2025-01-25 13:30:03 +0800

    minor fixes

--- PRG2-T13-01/LWTTFlight.cs
+++ PRG2-T13-01/LWTTFlight.cs
@@ -7,7 +7,7 @@ namespace PRG2_T13_01
         public double RequestFee { get; set; }
 
         public LWTTFlight(string fn, string o, string d, DateTime et, double rf = 500) 
-            : base(fn, o, d, et, s)
+            : base(fn, o, d, et)
         {
             RequestFee = rf;
         }
