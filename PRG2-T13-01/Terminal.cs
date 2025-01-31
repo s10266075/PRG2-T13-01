8e6b9f9e0e7cb7281a4a8b49377c00271bba676d
Day Gene Low <s10266075@connect.np.edu.sg>
2025-01-31 22:29:02 +0800

    fix the indentations

--- PRG2-T13-01/Terminal.cs
+++ PRG2-T13-01/Terminal.cs
@@ -157,7 +157,7 @@ namespace PRG2_T13_01
                     }
                     Console.WriteLine($"Original subtotal: ${a.CalculateFees().ToString("F2")}");
                     Console.WriteLine($"Total discount: ${discount.ToString("F2")}");
-                    Console.WriteLine($"Total: ${(a.CalculateFees()-discount).ToString("F2")}");
+                    Console.WriteLine($"Total: ${(a.CalculateFees() - discount).ToString("F2")}");
                 }
                 else
                 {
@@ -239,16 +239,7 @@ namespace PRG2_T13_01
                     }
                 }
             }
-            foreach (Flight flight in Flights.Values)
-            {
-                if (flight.ExpectedTime.Hour < 11 || flight.ExpectedTime.Hour > 21)
-                {
-                    discount += (110 + ((flight.CalculateFees() + 800) * 0.03));
-                }
-            }
-            return discount;
         }
 
-        }
     }
 }
